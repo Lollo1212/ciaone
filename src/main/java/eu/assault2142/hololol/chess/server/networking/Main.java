1c332eb3dd149a50615f3e274561d0a68c25fb02
jojo <jojowi@gmx.net>
2016-06-04 11:16:42 +0200

    Refactoring in (Server)Main

--- src/main/java/eu/assault2142/hololol/chess/server/networking/Main.java
+++ src/main/java/eu/assault2142/hololol/chess/server/networking/Main.java
@@ -1,7 +1,6 @@
-package eu.assault2142.hololol.chess.server.game;
+package eu.assault2142.hololol.chess.server.networking;
 
 import eu.assault2142.hololol.chess.game.Settings;
-import eu.assault2142.hololol.chess.server.networking.Server;
 import eu.assault2142.hololol.chess.server.util.Log;
 import eu.assault2142.hololol.chess.server.util.Time;
 import java.io.IOException;
@@ -13,7 +12,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        new Log("main" + Time.getTime());
+        Log log = new Log("main" + Time.getTime());
         Settings.init("default");
         new Server().init();
     }
