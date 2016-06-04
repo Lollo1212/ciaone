8937e174d794ee46fefbf6f1ccb06a9e54501080
jojo <jojowi@gmx.net>
2016-06-04 11:25:59 +0200

    Refactoring in Store

--- src/main/java/eu/assault2142/hololol/chess/server/util/Store.java
+++ src/main/java/eu/assault2142/hololol/chess/server/util/Store.java
@@ -31,103 +31,17 @@ public class Store {
     private KVStoreConfig config;
     private KVStore store;
 
+    public static void main(String[] args) {
+        Store store = new Store();
+        store.put("server", "nextid", "0");
+        System.out.println(store.get("server", "nextid"));
+    }
+
     /**
      * Creates a new Store
      */
     public Store() {
         init();
-    }
-
-    /**
-     * Initializes the Connection to the Database
-     */
-    private void init() {
-        config = new KVStoreConfig(STORE, HOST);
-        store = KVStoreFactory.getStore(config);
-    }
-
-    /**
-     * Puts a new Value to the Database
-     *
-     * @param majorkey the key path
-     * @param minorkey the subkey
-     * @param value the value
-     */
-    private void put(String majorkey, String minorkey, String value) {
-        try {
-            store.put(Key.createKey(majorkey, minorkey), Value.createValue(value.getBytes("utf-8")));
-        } catch (UnsupportedEncodingException ex) {
-            Logger.getLogger(Store.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    /**
-     * Retrieves a Value from the Database
-     *
-     * @param majorkey the key path
-     * @param minorkey the subkey
-     * @return the value
-     */
-    private String get(String majorkey, String minorkey) {
-        ValueVersion get = store.get(Key.createKey(majorkey, minorkey));
-        try {
-            return get == null ? null : new String(get.getValue().toByteArray(), "utf-8").trim();
-        } catch (UnsupportedEncodingException ex) {
-            Logger.getLogger(Store.class.getName()).log(Level.SEVERE, null, ex);
-            return null;
-        }
-    }
-
-    /**
-     * Removes a Value from the Database
-     *
-     * @param majorkey the key path
-     * @param minorkey the subkey
-     */
-    private void remove(String majorkey, String minorkey) {
-        store.delete(Key.createKey(majorkey, minorkey));
-    }
-
-    /**
-     * Retrieves a set of values from the Database
-     *
-     * @param majorkey the key path
-     * @param minorkeyfrom the start-subkey
-     * @param minorkeyto the end-subkey
-     * @return a Collection of the Values
-     */
-    private List<String> multiGet(String majorkey, String minorkeyfrom, String minorkeyto) {
-        SortedMap<Key, ValueVersion> multiGet = store.multiGet(Key.createKey(majorkey), new KeyRange(minorkeyfrom, true, minorkeyto, true), Depth.PARENT_AND_DESCENDANTS);
-        LinkedList<String> values = new LinkedList();
-        multiGet.forEach((Key k, ValueVersion vv) -> {
-            values.add(new String(vv.getValue().getValue()).trim());
-        });
-        return values;
-    }
-
-    /**
-     * Creates a new User
-     *
-     * @param name the username
-     * @param pass the password
-     * @return the id of the user
-     * @throws
-     * eu.assault2142.hololol.chess.server.exceptions.UsernameNotFreeException
-     */
-    public int createNewUser(String name, String pass) throws UsernameNotFreeException {
-        String id = get("server", "nextid");
-        if (!nameFree(name)) {
-            throw new UsernameNotFreeException(name);
-        }
-        put(id, "username", name);
-        put("server/names", name, id);
-        String hashed = BCrypt.hashpw(pass, BCrypt.gensalt(12));
-        put(id, "password", hashed);
-        id = id.trim();
-        int i = Integer.parseInt(id);
-        i++;
-        put("server", "nextid", i + "");
-        return i;
     }
 
     /**
@@ -146,19 +60,6 @@ public class Store {
     }
 
     /**
-     * Add a new undelivered message
-     *
-     * @param uid the recipient's id
-     * @param msg the message
-     * @throws
-     * eu.assault2142.hololol.chess.server.exceptions.UnknownUserException
-     */
-    public void addMessage(int uid, String msg) throws UnknownUserException {
-        checkID(uid);
-        put(uid + "/pendingmsgs", msg, msg);
-    }
-
-    /**
      * Add a new pending friend-request
      *
      * @param uid the sender's id
@@ -173,30 +74,16 @@ public class Store {
     }
 
     /**
-     * Retrieve a user from the store
+     * Add a new undelivered message
      *
-     * @param id the user's id
-     * @return the user or null if it does not exist
+     * @param uid the recipient's id
+     * @param msg the message
      * @throws
      * eu.assault2142.hololol.chess.server.exceptions.UnknownUserException
      */
-    public User getUser(int id) throws UnknownUserException {
-        checkID(id);
-        String name = getName(id);
-        return new User(name, getFriends(id), id);
-    }
-
-    /**
-     * Retrieve a username from the store
-     *
-     * @param id the user's id
-     * @return the username or null if it does not exist
-     * @throws
-     * eu.assault2142.hololol.chess.server.exceptions.UnknownUserException
-     */
-    public String getName(int id) throws UnknownUserException {
-        checkID(id);
-        return get(id + "", "username");
+    public void addMessage(int uid, String msg) throws UnknownUserException {
+        checkID(uid);
+        put(uid + "/pendingmsgs", msg, msg);
     }
 
     /**
@@ -225,50 +112,41 @@ public class Store {
     }
 
     /**
-     * Retrieve all unread messages
+     * Creates a new User
      *
-     * @param uid the user's id
-     * @return a list with unread messages
+     * @param name the username
+     * @param pass the password
+     * @return the id of the user
      * @throws
-     * eu.assault2142.hololol.chess.server.exceptions.UnknownUserException
+     * eu.assault2142.hololol.chess.server.exceptions.UsernameNotFreeException
      */
-    public List<String> getMessages(int uid) throws UnknownUserException {
-        checkID(uid);
-        return multiGet(uid + "/pendingmsgs", "0", null);
+    public int createNewUser(String name, String pass) throws UsernameNotFreeException {
+        String id = get("server", "nextid");
+        if (!nameFree(name)) {
+            throw new UsernameNotFreeException(name);
+        }
+        put(id, "username", name);
+        put("server/names", name, id);
+        String hashed = BCrypt.hashpw(pass, BCrypt.gensalt(12));
+        put(id, "password", hashed);
+        id = id.trim();
+        int i = Integer.parseInt(id);
+        i++;
+        put("server", "nextid", i + "");
+        return i;
     }
 
     /**
-     * Retrieves all pending friend-requests
+     * Remove a pending message
      *
-     * @param uid the user's id
-     * @return a list with pending requests
+     * @param id the id of the recipient
+     * @param msg the message
      * @throws
      * eu.assault2142.hololol.chess.server.exceptions.UnknownUserException
      */
-    public List<Integer> getRequests(int uid) throws UnknownUserException {
-        checkID(uid);
-        List<String> requests = multiGet(uid + "/pendingrequests", "0", null);
-        List<Integer> req = new LinkedList();
-        for (String str : requests) {
-            req.add(Integer.parseInt(str));
-        }
-        return req;
-    }
-
-    /**
-     * Retrieves the user-ID
-     *
-     * @param name the name of the user
-     * @return the ID
-     * @throws
-     * eu.assault2142.hololol.chess.server.exceptions.UnknownUserException
-     */
-    public int getID(String name) throws UnknownUserException {
-        String id = get("server/names", name);
-        if (id == null) {
-            throw new UnknownUserException(name);
-        }
-        return Integer.parseInt(id);
+    public void deleteMessage(int id, String msg) throws UnknownUserException {
+        checkID(id);
+        remove(id + "/pendingmsgs", msg);
     }
 
     /**
@@ -291,6 +169,80 @@ public class Store {
     }
 
     /**
+     * Retrieves the user-ID
+     *
+     * @param name the name of the user
+     * @return the ID
+     * @throws
+     * eu.assault2142.hololol.chess.server.exceptions.UnknownUserException
+     */
+    public int getID(String name) throws UnknownUserException {
+        String id = get("server/names", name);
+        if (id == null) {
+            throw new UnknownUserException(name);
+        }
+        return Integer.parseInt(id);
+    }
+
+    /**
+     * Retrieve all unread messages
+     *
+     * @param uid the user's id
+     * @return a list with unread messages
+     * @throws
+     * eu.assault2142.hololol.chess.server.exceptions.UnknownUserException
+     */
+    public List<String> getMessages(int uid) throws UnknownUserException {
+        checkID(uid);
+        return multiGet(uid + "/pendingmsgs", "0", null);
+    }
+
+    /**
+     * Retrieve a username from the store
+     *
+     * @param id the user's id
+     * @return the username or null if it does not exist
+     * @throws
+     * eu.assault2142.hololol.chess.server.exceptions.UnknownUserException
+     */
+    public String getName(int id) throws UnknownUserException {
+        checkID(id);
+        return get(id + "", "username");
+    }
+
+    /**
+     * Retrieves all pending friend-requests
+     *
+     * @param uid the user's id
+     * @return a list with pending requests
+     * @throws
+     * eu.assault2142.hololol.chess.server.exceptions.UnknownUserException
+     */
+    public List<Integer> getRequests(int uid) throws UnknownUserException {
+        checkID(uid);
+        List<String> requests = multiGet(uid + "/pendingrequests", "0", null);
+        List<Integer> req = new LinkedList();
+        for (String str : requests) {
+            req.add(Integer.parseInt(str));
+        }
+        return req;
+    }
+
+    /**
+     * Retrieve a user from the store
+     *
+     * @param id the user's id
+     * @return the user or null if it does not exist
+     * @throws
+     * eu.assault2142.hololol.chess.server.exceptions.UnknownUserException
+     */
+    public User getUser(int id) throws UnknownUserException {
+        checkID(id);
+        String name = getName(id);
+        return new User(name, getFriends(id), id);
+    }
+
+    /**
      * Check if a username is free to take
      *
      * @param name the name to check
@@ -299,41 +251,6 @@ public class Store {
     public boolean nameFree(String name) {
         String s = get("server/names", name);
         return s == null;
-    }
-
-    /**
-     * Change the name of a given user
-     *
-     * @param id the user's id
-     * @param name the new username
-     * @throws
-     * eu.assault2142.hololol.chess.server.exceptions.UnknownUserException
-     * @throws
-     * eu.assault2142.hololol.chess.server.exceptions.UsernameNotFreeException
-     */
-    public void setUsername(int id, String name) throws UnknownUserException, UsernameNotFreeException {
-        checkID(id);
-        if (!nameFree(name)) {
-            throw new UsernameNotFreeException(name);
-        }
-        String oldname = getName(id);
-        put(id + "", "username", name);
-        put("server/names", name, id + "");
-        remove("server/names", oldname);
-    }
-
-    /**
-     * Change the password of a given user
-     *
-     * @param id the user's id
-     * @param pass the new password
-     * @throws
-     * eu.assault2142.hololol.chess.server.exceptions.UnknownUserException
-     */
-    public void setPassword(int id, String pass) throws UnknownUserException {
-        checkID(id);
-        String hashed = BCrypt.hashpw(pass, BCrypt.gensalt(12));
-        put(id + "", "password", hashed);
     }
 
     /**
@@ -380,22 +297,105 @@ public class Store {
         return get(id0 + "/pendingrequests", id + "") != null;
     }
 
-    public static void main(String[] args) {
-        Store store = new Store();
-        store.put("server", "nextid", "0");
-        System.out.println(store.get("server", "nextid"));
-    }
-
     /**
-     * Remove a pending message
+     * Change the password of a given user
      *
-     * @param id the id of the recipient
-     * @param msg the message
+     * @param id the user's id
+     * @param pass the new password
      * @throws
      * eu.assault2142.hololol.chess.server.exceptions.UnknownUserException
      */
-    public void deleteMessage(int id, String msg) throws UnknownUserException {
+    public void setPassword(int id, String pass) throws UnknownUserException {
         checkID(id);
-        remove(id + "/pendingmsgs", msg);
+        String hashed = BCrypt.hashpw(pass, BCrypt.gensalt(12));
+        put(id + "", "password", hashed);
+    }
+
+    /**
+     * Change the name of a given user
+     *
+     * @param id the user's id
+     * @param name the new username
+     * @throws
+     * eu.assault2142.hololol.chess.server.exceptions.UnknownUserException
+     * @throws
+     * eu.assault2142.hololol.chess.server.exceptions.UsernameNotFreeException
+     */
+    public void setUsername(int id, String name) throws UnknownUserException, UsernameNotFreeException {
+        checkID(id);
+        if (!nameFree(name)) {
+            throw new UsernameNotFreeException(name);
+        }
+        String oldname = getName(id);
+        put(id + "", "username", name);
+        put("server/names", name, id + "");
+        remove("server/names", oldname);
+    }
+
+    /**
+     * Retrieves a Value from the Database
+     *
+     * @param majorkey the key path
+     * @param minorkey the subkey
+     * @return the value
+     */
+    private String get(String majorkey, String minorkey) {
+        ValueVersion get = store.get(Key.createKey(majorkey, minorkey));
+        try {
+            return get == null ? null : new String(get.getValue().toByteArray(), "utf-8").trim();
+        } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(Store.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+
+    /**
+     * Initializes the Connection to the Database
+     */
+    private void init() {
+        config = new KVStoreConfig(STORE, HOST);
+        store = KVStoreFactory.getStore(config);
+    }
+
+    /**
+     * Retrieves a set of values from the Database
+     *
+     * @param majorkey the key path
+     * @param minorkeyfrom the start-subkey
+     * @param minorkeyto the end-subkey
+     * @return a Collection of the Values
+     */
+    private List<String> multiGet(String majorkey, String minorkeyfrom, String minorkeyto) {
+        SortedMap<Key, ValueVersion> multiGet = store.multiGet(Key.createKey(majorkey), new KeyRange(minorkeyfrom, true, minorkeyto, true), Depth.PARENT_AND_DESCENDANTS);
+        LinkedList<String> values = new LinkedList();
+        multiGet.forEach((Key k, ValueVersion vv) -> {
+            values.add(new String(vv.getValue().getValue()).trim());
+        });
+        return values;
+    }
+
+    /**
+     * Puts a new Value to the Database
+     *
+     * @param majorkey the key path
+     * @param minorkey the subkey
+     * @param value the value
+     */
+    private void put(String majorkey, String minorkey, String value) {
+        try {
+            store.put(Key.createKey(majorkey, minorkey), Value.createValue(value.getBytes("utf-8")));
+        } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(Store.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Removes a Value from the Database
+     *
+     * @param majorkey the key path
+     * @param minorkey the subkey
+     */
+    private void remove(String majorkey, String minorkey) {
+        store.delete(Key.createKey(majorkey, minorkey));
     }
 }
